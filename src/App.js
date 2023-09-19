577ebab2d26d1f1b436fd3edbe67d1a1ad5bc1d4
Erik Mekiassen <erik.mekiassen@spk.no>
2023-09-19 15:18:04 +0200

    added the working dir to git with an example from p5.js to show rendering works

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Script from 'react-script-tag-18';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Script type="text/javascript" src="sketch.js" />
+    </div>
+  );
+}
+
+export default App;
